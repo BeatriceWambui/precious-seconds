d5ac9dda483b0ce04347e3b9d475b81998f569b4
Wachira Beatrice <BeatriceWambui>
2019-09-18 16:32:57 +0300

    import pitch form models and create a def for new_pitches so tthat a user can enter a pitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,6 +5,6 @@ from wtforms import ValidationError
 
 class PitchForm(FlaskForm):
     title = StringField('Title',validators=[Required()])
-    description = TextAreaField('Would you love to share a pitch?',validators=[Required()])
+    description = TextAreaField('please share a pitch',validators=[Required()])
     category = RadioField('Label', choices=[('pickup lines','pickup lines'),('interview pitch','interview pitch'),('product pitch', 'product pitch'),('promotion pitch','promotion pitch')])
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,22 @@
-from flask import render_template
-from flask_login import login_required
+from flask import render_template,redirect,url_for
+from flask_login import login_required,current_user
 from . import main
+from .. import db
+from app.models import Pitch
+from .forms import PitchForm
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    all_pitches = Pitch.query.all()
+    return render_template('index.html',all_pitches =all_pitches )
 
-@main.route('/pitches/new/',methods = ['GET','POST'])
+@main.route('/pitches/new',methods = ['GET','POST'])
 @login_required
-def new_pitch(id): 
-    return render_template('.html')
+def new_pitch(): 
+    form = PitchForm()
+    if form.validate_on_submit():
+        user_id= current_user._get_current_object().id
+        pitch = Pitch(category = form.category.data,title=form.title.data,description=form.description.data,user_id=user_id)
+        pitch.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html',form=form)
