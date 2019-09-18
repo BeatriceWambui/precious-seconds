ca865aaa754ec680bbb857a11f2edb85a7e29fd4
Wachira Beatrice <BeatriceWambui>
2019-09-18 14:55:22 +0300

    import forms in the auth init.py and import login manager and add session protect for security

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +19,7 @@ def create_app():
     app.register_blueprint(auth)
     app.register_blueprint(main)
     db.init_app(app)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+
     return app
